e7c949fe9d641d645485ddcf2e660f5c88ee70dc
souviknath94 <souvik_nath1994@outlook.com>
2020-07-29 01:39:02 +0530

    First commit

---
+++ darwin/darwin.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from gtts import gTTS
+import speech_recognition as sr
+import re
+import time
+# import pyttsx3 
+from selenium import webdriver
+import datetime
+import wikipedia
+import webbrowser
+import os
+import time
+import subprocess
+from ecapture import ecapture as ec
+import wolframalpha
+import requests
+import logging
+
+
+logging.info('Loading your personal AI assistant - Darwin')
+
+#Define virtual AI voice properties using pyttsx3
---
+++ docs/index.md
@@ -0,0 +1,21 @@
+# Darwin Virtual Assistant
+## Project layout
+
+    mkdocs.yml    # The configuration file.
+    docs/
+        index.md  # The documentation homepage.
+    site/
+        css/
+        fonts/
+        img/
+        js
+        search/
+        404.html
+        index.html
+        sitemap.xml
+        sitemap.xml.gz
+    __init__.py
+    darwin.py
+    mkdocs.yml
+    requirements.txt
+        
