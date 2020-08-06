b6ea0c7729f5f3a29882a6053d2b9c616438faca
Souvik Nath <souviknath@Souvik.local>
2020-08-07 00:09:31 +0530

    Added youtube search functionalities in activities.py

--- darwin/imports.py
+++ darwin/imports.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os, re, json, time, urllib, requests, datetime, wikipedia, webbrowser, subprocess, logging, wolframalpha
 from gtts import gTTS
-import speech_recognition as sr
-import re
-import time 
-from selenium import webdriver
-import datetime
-import wikipedia
-import webbrowser
-import os
-import time
-import subprocess
-from ecapture import ecapture as ec
-import wolframalpha
-import requests
-import logging
 from pygame import mixer
+from selenium import webdriver
+import speech_recognition as sr
+from ecapture import ecapture as ec
--- darwin/paths.py
+++ darwin/paths.py
@@ -3,5 +3,5 @@
 
 from darwin.imports import os
 
-__dirname__ = os.path.dirname(os.path.abspath("__file__"))
-__audio_files__ = __dirname__ + '/audio/'
+__dirname__ = os.path.dirname(os.path.abspath(__file__))
+__audio_files__ = os.path.join(__dirname__, 'audio/')
